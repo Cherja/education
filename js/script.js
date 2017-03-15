90305d3a0924af0a11ab0909ef9437617d4c6180
cherja <vitalytrust2016@mail.ru>
2017-03-15 22:02:54 +0300

    Сделал тултип и провернул фокус по нем.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(function () {
     $('#navigationmap button[data-toggle="popover"]').popover();
-    $('#atiTooltip').tooltip();
-    $('#galleryTooltip').tooltip();
+    $('#navigationmaptp button[data-toggle="tooltip"]').tooltip();
 });
