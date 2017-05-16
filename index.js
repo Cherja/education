f8df1e5538eb144adb4fd512a11320173f61be98
Виталий Чернявский <4irik91@mail.ru>
2017-05-16 14:41:43 +0300

    Убрал замечания#11

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
+// 1.Проверка на Enter и отключение ввода букв.
 code.onkeypress = function(e) {
   var charCode = e.charCode;
 
   if (charCode == 13) {
     saveCode(code.value);
     clearInput();
-  };
-
+  }
   if (charCode >= 48 && charCode <= 57) {
     return true;
   } else {
     return false;
   }
-
-
 }
+//2.Функция сохранения номера
+function saveCode(value) {
+  //result.innerHTML += "<li>" + value + "</li>";
+  var elem = document.createElement("li"),
+    content = document.createTextNode(value);
+
+    if (value == '')return false;
+
+  elem.appendChild(content);
+  result.parentNode.appendChild(elem);
+}
+//3.Функция очистки инпута.
+function clearInput() {
+  code.value = ''
+}
+//4.Проверка строки на изменение и при количестве 12 символов добавление в кодлист.
 code.oninput = function() {
   var value = this.value;
 
@@ -22,22 +36,11 @@ code.oninput = function() {
     clearInput();
   }
 }
-
-function saveCode(value) {
-  //result.innerHTML += "<li>" + value + "</li>";
-  var elem = document.createElement("li"),
-    content = document.createTextNode(value)
-    if (value == '') {
-      return false;
-    }
-  elem.appendChild(content);
-  result.parentNode.appendChild(elem);
-};
-
-function clearInput() {
-  code.value = ''
+//5.Постоянный автофокус на инпуте.
+document.onclick = function(){
+  code.focus();
 }
 
-document.onclick = function(){
-  document.getElementById('code').focus();
+code.onclick = function () {
+  clearInput();
 }
