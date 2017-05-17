f4517b4b82930713d6ff40eef37b7d6e10aa6756
Виталий Чернявский <4irik91@mail.ru>
2017-05-17 14:18:33 +0300

    Пропадающий submit#11

--- index.js
+++ index.js
@@ -26,10 +26,13 @@ function saveCode(value) {
 //3.Функция очистки инпута.
 function clearInput() {
   code.value = ''
+  sub.style.display='none';
 }
 //4.Проверка строки на изменение и при количестве 12 символов добавление в кодлист.
 code.oninput = function() {
   var value = this.value;
+  // if (!value)
+  sub.style.display='inline';
 
   if (value.length == 12) {
     saveCode(value);
