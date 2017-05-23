1d45e548f77ba456bd6099f14b02bfa57508deab
Виталий Чернявский <4irik91@mail.ru>
2017-05-23 12:56:34 +0300

    Начало JS

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // calculationcard - калькуляция в карточке
+
+input.oninput = function() {
+  var value = this.value;
+  if (value.length >= 1) {
+
+  }
+}
+// сложение -addition
+function addition() {
+  price.
+
+}
