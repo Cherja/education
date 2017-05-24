083372ed8609642975cf6ccabcc3ab64cfeb66e8
jonkofee <jonkofee@icloud.com>
2017-05-24 21:00:55 +0300

    #14 получение общей цены при нажатии кнопки

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // calculationcard - калькуляция в карточке
 
 function changeCountInput(elem, cardId) {
-  value = parseInt(elem.value),
+  var value = parseInt(elem.value),
     price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
     totalPrice = (value * price).toFixed(2);
   document.querySelector('.card[data-id="' + cardId + '"] .total_price').textContent = '($' + totalPrice + ')';
 }
 
 function pressingbutton(cardId) {
-  // var value = parseInt(document.querySelector('.card[data-id="' + cardId + '"] .totalnymber').textContent),
-  //   price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
-  //   totalPrice = (value * price).toFixed(2);
-  r = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
-    b = (value * r).toFixed(2);
-  console.log(b);
+  var value = parseInt(document.querySelector('.card[data-id="' + cardId + '"] input').value),
+    price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent)
+  totalPrice = (value * price).toFixed(2);
+  // r = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
+  //   b = (value * r).toFixed(2);
+  console.log(totalPrice);
 
 
 }
