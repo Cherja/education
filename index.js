de40ae4372aa11f91a4abb7a1d763b4886faca8e
Виталий Чернявский <4irik91@mail.ru>
2017-05-24 16:29:33 +0300

    Убрал VAR сдела количество глобальным,знаю,что неправильно,но не могу чезез функцию кнопки достать значение(((((((

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // calculationcard - калькуляция в карточке
 
 function changeCountInput(elem, cardId) {
-  var value = parseInt(elem.value),
+  value = parseInt(elem.value),
     price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
     totalPrice = (value * price).toFixed(2);
-
   document.querySelector('.card[data-id="' + cardId + '"] .total_price').textContent = '($' + totalPrice + ')';
+}
+
+function pressingbutton(cardId) {
+  // var value = parseInt(document.querySelector('.card[data-id="' + cardId + '"] .totalnymber').textContent),
+  //   price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
+  //   totalPrice = (value * price).toFixed(2);
+  r = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
+    b = (value * r).toFixed(2);
+  console.log(b);
+
+
 }
