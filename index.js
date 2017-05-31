4ddf97e1a4646126d8823562b330bf708cd69f41
Виталий Чернявский <4irik91@mail.ru>
2017-05-31 22:07:01 +0300

    oninput через js

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 // calculationcard - калькуляция в карточке
 
+var array = document.getElementsByTagName('input');
+for (var i = 0; i < array.length; i++) {
+  var enik = array[i];
+  enik.oninput = function(elem) {
+    var cardId = elem.getAttribute('data-id');
+    console.log(cardId);
+    return;
+
+    var value = elem.valueAsNumber,
+      price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
+      totalPrice = (value * price).toFixed(2);
+
+    document.querySelector('.card[data-id="' + cardId + '"] .total_price').textContent = '($' + totalPrice + ')';
+  }
+  console.log(enik);
+}
+
+// console.log(document.getElementsByTagName('input'));
+
+
+
 function changeCountInput(elem, cardId) {
-  var value = parseInt(elem.value),
-    price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
-    totalPrice = (value * price).toFixed(2);
-  document.querySelector('.card[data-id="' + cardId + '"] .total_price').textContent = '($' + totalPrice + ')';
+  // console.dir(document.querySelector('.card[data-id="' + cardId + '"] .rate'));
+
 }
 
 function pressingbutton(cardId) {
-  var value = parseInt(document.querySelector('.card[data-id="' + cardId + '"] input').value),
+  var value = document.querySelector('.card[data-id="' + cardId + '"] input').valueAsNumber,
     price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
     name = document.querySelector('.card[data-id="' + cardId + '"] .name').textContent,
-    totalPrice = (value * price).toFixed(2);
+    totalPrice = parseFloat((value * price).toFixed(2));
 
   var elem = {
     name: name,
@@ -23,10 +42,10 @@ function pressingbutton(cardId) {
   tableAddRow(elem);
 }
 
-function tableAddRow(row) {
+function tableAddRow(elem) {
 
   var elem = document.createElement('tr');
-  elem.innerHTML = '<td>' + row.name + '</td><td>' + row.count + '</td><td>' + row.totalPrice + '</td><td></td>';
+  elem.innerHTML = '<td>' + elem.name + '</td><td>' + elem.count + '</td><td>' + elem.totalPrice + '</td><td></td>';
 
 
   ttr.appendChild(elem)
