18742ea8e9e307ef251bd19904b6eb8bae6d52ef
Andrey Klimash <andrey.klimash@gmail.com>
2017-05-31 22:25:37 +0300

    Изменил получение элемента в колбэке на событие

--- index.js
+++ index.js
@@ -2,19 +2,17 @@
 
 var array = document.getElementsByTagName('input');
 for (var i = 0; i < array.length; i++) {
-  var enik = array[i];
-  enik.oninput = function(elem) {
-    var cardId = elem.getAttribute('data-id');
-    console.log(cardId);
-    return;
+    var enik = array[i];
+    enik.oninput = function(e) {
+        var elem = e.target,
+            cardId = elem.getAttribute('data-id'),
+            value = elem.valueAsNumber,
+            price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
+            totalPrice = (value * price).toFixed(2);
 
-    var value = elem.valueAsNumber,
-      price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
-      totalPrice = (value * price).toFixed(2);
-
-    document.querySelector('.card[data-id="' + cardId + '"] .total_price').textContent = '($' + totalPrice + ')';
-  }
-  console.log(enik);
+        document.querySelector('.card[data-id="' + cardId + '"] .total_price').textContent = '($' + totalPrice + ')';
+    }
+    console.log(enik);
 }
 
 // console.log(document.getElementsByTagName('input'));
@@ -22,31 +20,31 @@ for (var i = 0; i < array.length; i++) {
 
 
 function changeCountInput(elem, cardId) {
-  // console.dir(document.querySelector('.card[data-id="' + cardId + '"] .rate'));
+    // console.dir(document.querySelector('.card[data-id="' + cardId + '"] .rate'));
 
 }
 
 function pressingbutton(cardId) {
-  var value = document.querySelector('.card[data-id="' + cardId + '"] input').valueAsNumber,
-    price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
-    name = document.querySelector('.card[data-id="' + cardId + '"] .name').textContent,
-    totalPrice = parseFloat((value * price).toFixed(2));
+    var value = document.querySelector('.card[data-id="' + cardId + '"] input').valueAsNumber,
+        price = parseFloat(document.querySelector('.card[data-id="' + cardId + '"] .rate').textContent),
+        name = document.querySelector('.card[data-id="' + cardId + '"] .name').textContent,
+        totalPrice = parseFloat((value * price).toFixed(2));
 
-  var elem = {
-    name: name,
-    price: price,
-    count: value,
-    totalPrice: totalPrice
-  }
+    var elem = {
+        name: name,
+        price: price,
+        count: value,
+        totalPrice: totalPrice
+    }
 
-  tableAddRow(elem);
+    tableAddRow(elem);
 }
 
 function tableAddRow(elem) {
 
-  var elem = document.createElement('tr');
-  elem.innerHTML = '<td>' + elem.name + '</td><td>' + elem.count + '</td><td>' + elem.totalPrice + '</td><td></td>';
+    var elem = document.createElement('tr');
+    elem.innerHTML = '<td>' + elem.name + '</td><td>' + elem.count + '</td><td>' + elem.totalPrice + '</td><td></td>';
 
 
-  ttr.appendChild(elem)
+    ttr.appendChild(elem)
 }
