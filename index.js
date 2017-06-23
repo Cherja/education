3e28c9a77c3bb8b3a451ee4274938ca23938efbd
Виталий Чернявский <4irik91@mail.ru>
2017-06-23 17:56:09 +0400

    Выпадающее меню)

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("is-active");
 });
+
+
+var header = document.querySelector(".header");
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function() {
+  header.classList.toggle("active");
+});
